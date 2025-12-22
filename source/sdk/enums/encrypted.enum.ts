7d2caf2523c6ff5d03c9d79ab1a5ceecf065e25a
Protorians <129060085+protorians@users.noreply.github.com>
2025-12-22 14:12:19 +0000

    feat: add enums for SDK modularization
    - Added multiple enums (`HttpMethodEnum`, `ParamTypeEnum`, `HashAlgoEnum`, `EventMessageEnum`, `TimestampEnum`) to streamline SDK code structure.
    - Exported all enums through `index.ts` for centralized access.

---
+++ source/sdk/enums/encrypted.enum.ts
@@ -0,0 +1,13 @@
+export enum HashAlgoEnum {
+    ARGON2ID = 'ARGON2ID',
+    BCRYPT = 'BCRYPT',
+    PBKDF2 = 'PBKDF2',
+    SCRYPT = 'SCRYPT',
+    SHA256 = 'SHA256',
+    SHA512 = 'SHA512',
+    MD5 = 'MD5',
+    RIPEMD160 = 'RIPEMD160',
+    BLAKE2B = 'BLAKE2B',
+    SHA3_256 = 'SHA3_256',
+    SHA3_512 = 'SHA3_512',
+}
---
+++ source/sdk/enums/http-parameters.enum.ts
@@ -0,0 +1,11 @@
+export enum ParamTypeEnum {
+    APP = "application",
+    PARAM = "param",
+    BODY = "body",
+    QUERY = "query",
+    HEADER = "header",
+    REQ = "req",
+    RES = "res",
+    UPLOAD_FILE = "upload",
+    CUSTOM = "custom",
+}
