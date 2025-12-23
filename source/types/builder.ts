46a003c7c59bfbe480cb64dc280c1771f77c3f0b
Protorians <129060085+protorians@users.noreply.github.com>
2025-12-23 09:47:58 +0000

    chore: replace `caches.type.ts` with `hmr.ts`, update HMR types, and extend `builder` and `server` interfaces to support HMR

--- source/types/builder.ts
+++ source/types/builder.ts
@@ -1,4 +1,6 @@
 import type {BuildContext, BuildOptions} from "esbuild";
+import {HmrInterface} from "@/types/hmr";
+import {ISignalStack} from "@protorians/core";
 
 export interface BuildCommandOptions {
     develop?: boolean;
@@ -12,20 +14,31 @@ export interface BuilderConfig {
 export type BuilderBootCallable = (builder: BuilderInterface) => Promise<void>
 
 export interface BuilderInterface {
-    readonly workdir: string,
-    readonly options: BuilderConfig
+    readonly workdir: string;
+    readonly options: BuilderConfig;
+    readonly hmr: HmrInterface;
+    readonly signal: ISignalStack<BuilderSignalMap>;
+
     get context(): BuildContext<BuildOptions> | null;
+
     get source(): string | null;
+
     get out(): string | null;
+
     get bootstrapper(): string | null;
+
     get bootstrapperIndex(): string | null;
+
     get baseConfig(): BuildOptions;
+
     prepare(): Promise<this>;
+
     boot(): Promise<any>;
+
     start(callable?: BuilderBootCallable): Promise<this>;
 }
 
 
-export interface BuilderSignalMap{
+export interface BuilderSignalMap {
     ready?: undefined;
 }
