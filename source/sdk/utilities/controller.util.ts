1b6f104300a392ed22e73a6fbb276378036c304e
Protorians <protorians.dev@gmail.com>
2026-06-04 15:42:08 +0000

    remove: deprecated core and SDK modules no longer in use

--- source/sdk/utilities/controller.util.ts
+++
@@ -1,8 +0,0 @@
-export function isControllerFile(filename: string) {
-    return [
-        filename.endsWith(".controller.ts"),
-        filename.endsWith(".controller.js"),
-        filename.endsWith(".controller.mjs"),
-        filename.endsWith(".controller.cjs"),
-    ].some(Boolean)
-}
