6db145be6592a0280378bf9136fbb29161054269
Protorians <protorians.dev@gmail.com>
2026-07-21 16:04:14 +0000

    feat: enhance HttpException class with error code and method chaining
    - Added `errorCode` property to `HttpException`
    - Introduced method chaining with `setStatus`, `setErrorCode`, and `setMessage`
    - Updated `render` method to include `errorCode` in the response output

--- source/framework/exceptions/http-exception.ts
+++ source/framework/exceptions/http-exception.ts
@@ -7,14 +7,31 @@ export class HttpException extends Error {
     constructor(
         error: string | HttpResponseBaseInterface | Error,
         public statusCode: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+        public errorCode: number | string | symbol | undefined = undefined,
     ) {
         super(typeof error === 'string' ? error : error.message);
+    }
+
+    setStatus(statusCode: HttpStatus) {
+        this.statusCode = statusCode;
+        return this;
+    }
+
+    setErrorCode(code: number | string | symbol | undefined) {
+        this.errorCode = code;
+        return this;
+    }
+
+    setMessage(error: string | HttpResponseBaseInterface | Error) {
+        this.message = typeof error === 'string' ? error : error.message;
+        return this;
     }
 
     render() {
         return {
             message: this.message,
             statusCode: this.statusCode,
+            errorCode: this.errorCode,
             error: true,
             stack: Raiton.thread?.builder?.options?.serve
                 ? this.stack?.split('\n').map(e => e.trim())
