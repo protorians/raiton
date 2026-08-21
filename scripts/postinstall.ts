041d041b0a062aebd6acf5de0918045b4f8453ec
Protorians <protorians.dev@gmail.com>
2026-08-21 02:51:07 +0000

    refactor: migrate postinstall script to TypeScript and update package.json accordingly

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 const bin = path.resolve(__dirname, 'source/bin/index.ts');
 const workdir = path.dirname(bin);
