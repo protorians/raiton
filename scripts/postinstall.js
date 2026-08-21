8a819e44d71e0208b00457264e668fc4570cd573
Protorians <protorians.dev@gmail.com>
2026-08-21 02:49:51 +0000

    feat: add postinstall script for Windows compatibility and bin file permissions setup

---
+++ scripts/postinstall.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const bin = path.resolve(__dirname, 'source/bin/index.ts');
+const workdir = path.dirname(bin);
+const base = path.basename(bin, '.ts');
+
+if (process.platform === 'win32') {
+  const cmdContent = `@IF EXIST "%~dp0\\node.exe" (
+  "%~dp0\\node.exe" "${bin}" %*
+) ELSE (
+  @SETLOCAL
+  @SET PATHEXT=%PATHEXT:;.JS;=;%
+  node "${bin}" %*
+)`;
+  const ps1Content = `#!/usr/bin/env pwsh
+$basedir=Split-Path $MyInvocation.MyCommand.Definition -Parent
+
+if (Test-Path "$basedir/node.exe") {
+  # Note: running setup.exe from the current directory may fail due to Windows Installer restrictions.
+  # See https://github.com
+  & "$basedir/node.exe" "$basedir/${path.relative(workdir, bin)}" $args
+} else {
+  & "node" "$basedir/${path.relative(workdir, bin)}" $args
+}`;
+
+  try {
+    fs.writeFileSync(path.join(workdir, `${base}.cmd`), cmdContent, 'utf-8');
+    fs.writeFileSync(path.join(workdir, `${base}.ps1`), ps1Content, 'utf-8');
+  } catch (err) {
+    console.error('Raiton Cli: Error create Windows files', err);
+  }
+
+} else {
+  fs.chmod(bin, '755', (err) => {
+    if (err) {
+      console.error('Raiton Cli: Error chmod', err);
+    }
+  });
+}
